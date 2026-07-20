251fffbbb94bf36ae01d6fee26f38097dc924f94
EDDIE/Quanwei CHEN <eddiecqw@gmail.com>
2026-07-20 13:04:22 +0800

    Refactor WebSocket URL handling in ChatRoom component
    Updated WebSocket URL initialization to use environment variable.

--- frontend_new/src/components/ChatRoom.jsx
+++ frontend_new/src/components/ChatRoom.jsx
@@ -4,8 +4,6 @@ import { Link } from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
 
 export function Home({ username ,onLogout}) {
-  /*const WS_URL = (window.location.protocol === 'https:' ? 'wss:' : 'ws:') + 
-               '//' + window.location.host + '/ws';*/
   const WS_URL = import.meta.env.VITE_WS_URL || 'ws://localhost:53840/ws';
   const navigate = useNavigate();
   const { sendJsonMessage, lastJsonMessage } = useWebSocket(WS_URL, {
@@ -185,3 +183,4 @@ export function Home({ username ,onLogout}) {
       </div>
     </div>
   );
+}
